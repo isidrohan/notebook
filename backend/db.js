c2c7213c0d8567f51417a72160668da07cf9c5c9
Siddharth Rohan <sidrohanme2@gmail.com>
2023-08-07 16:40:40 +0530

    change compass to atlas

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,8 @@
 const mongoose = require('mongoose');
-const mongoURI = "mongodb://127.0.0.1:27017/inotebook"
+// const mongoURI = "mongodb://127.0.0.1:27017/inotebook"
+const mongoURI = "mongodb+srv://sidrohanme2:sidnotebook@cluster0.wg3rnbn.mongodb.net/"
+
+
 
 const connectToMongo = ()=>{
     mongoose.connect(mongoURI)
